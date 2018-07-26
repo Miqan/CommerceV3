1d0206d7158e2c6abb547ef643d8df150713f203
Ahmet H SAHA <ahmethsaha@outlook.com>
2018-07-26 14:39:40 +0300

    Add project files.

---
+++ CommerceV3/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommerceV3.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceV3.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager= userManager;
+        }
+        public IActionResult Index()
+        {
+            var users = userManager.Users.ToList();
+            return View(users);
+        }
+        public async Task<IActionResult>Delete(string id)
+        {
+            var users = await userManager.FindByIdAsync(id);
+            await userManager.DeleteAsync(users);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CommerceV3/Data/Migrations/20180726103952_addPhoto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommerceV3.Data.Migrations
+{
+    public partial class addPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
